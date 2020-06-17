8f0d7d1aa3aa1001553820b541fd07fc014c4a56
a <a@qq.com>
2020-06-17 09:32:43 +0000

    5.1章挑战失败

--- src/utils/api.js
+++ src/utils/api.js
@@ -44,15 +44,90 @@ const login = async (params = {}) => {
 
 	if(authResponse.statusCode === 201) {
 		wepy.setStorageSync('access_token', authResponse.data.access_token)
-		wepy.setStorageSync('access_token_expired_at', new Date().getTime() + authResponse.data.expires_in *10)
+		wepy.setStorageSync('access_token_expired_at', (new Date()).getTime() + authResponse.data.expires_in * 1000)
 	}
 
 	return authResponse
 }
 
+const refreshToken = async (accessToken) => {
+	let refreshResponse = await wepy.request({
+		url: host + '/' + 'authorizations/current',
+		method: 'PUT',
+		header: {
+			'Authorizations' : 'Bearer ' + accessToken
+		}
+	})
+
+	if (refreshResponse.statusCode === 200) {
+		wepy.setStorageSync('access_token', refreshResponse.data.access_token)
+		wepy.setStorageSync('access_token_expired_at', (new Date()).getTime() + refreshResponse.data.expires_in * 1000)
+	}
+
+	return refreshResponse
+}	
+
+const getToken = async(options) => {
+	let accessToken = wepy.getStorageSync('access_token')
+	let expiredAt = wepy.getStorageSync('access_token_expired_at')
+
+	if(accessToken && (new Date()).getTime() > expiredAt) {
+		let refreshResponse = await refreshToken(accessToken)
+
+		if(refreshResponse.statusCode === 200) {
+			accessToken = refreshResponse.data.access_token
+		} else {
+			let authResponse = await login()
+			accessToken = authResponse.data.access_token
+		} 
+	}
+
+	return accessToken
+}
+
+const authRequest = async (options, showLoading = true) => {
+	if (typeof options === 'string') {
+		options = {
+			url: options
+		}
+	}
+
+	let accessToken = await getToken()
+
+	let header = options.header || {}
+	header.Authorization = 'Bearer ' + accessToken
+	options.header = header
+
+	return request(options, showLoading)
+}
+
+const logout = async (params = {}) => {
+	let accessToken = wepy.getStorageSync('access_token')
+
+	let logoutResponse = await wepy.request({
+		url: host + '/' + 'authorizations/current',
+		method: 'DELETE',
+		header: {
+			'Authorization': 'Bearer '+ accessToken
+		}
+	})
+
+	if(logoutResponse.statusCode === 204) {
+		wepy.clearStorage()
+	}
+	return logoutResponse
+}
+
+
+
+
+
 export default {
 	request,
-	login
+	authRequest,
+	refreshToken,
+	login,
+	logout
 }
 
 
